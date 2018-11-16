dc6d7fb42e0e366f0d9ed23eb0d073d588d3e521
Longri <www.longri.de@googlemail.com>
2018-11-16 13:32:09 +0100

    add install MC and TVHeadend

--- install.sh
+++ install.sh
@@ -15,47 +15,62 @@ read answer
 echo  Ihre Antwort war: $answer
 # if [ "$answer" = "j" ]
 if [ "$answer" != "n" ]
-  then apt-get install mc && 
-echo "deb http://packages.openmediavault.org/public arrakis main" > /etc/apt/sources.list.d/openmediavault.list &&
-sleep 5
-apt install --force-yes curl wget apt-transport-https dirmngr &&
-sleep 5
-gpg --recv-keys 7E7A6C592EF35D13 &&
-sleep 5
-gpg --export 7E7A6C592EF35D13 |apt-key add - &&
-sleep 5
-gpg --recv-keys 7E7A6C592EF35D13 &&
-sleep 5
-gpg --export 7E7A6C592EF35D13 |apt-key add - &&
-sleep 5
-gpg --recv-keys 24863F0C716B980B &&
-sleep 5
-gpg --export 24863F0C716B980B |apt-key add - &&
-sleep 5
-gpg --recv-keys 24863F0C716B980B &&
-sleep 5
-gpg --export 24863F0C716B980B |apt-key add - &&
-sleep 1
-apt update &&
-sleep 1
-apt-get install --force-yes openmediavault-keyring postfix ssl-cert &&
-sleep 1
-apt update &&
-sleep 1
-apt-get --yes --force-yes --allow-unauthenticated install openmediavault &&
-sleep 1
-wget http://omv-extras.org/openmediavault-omvextrasorg_latest_all4.deb
-sleep 1
-dpkg -i openmediavault-omvextrasorg_latest_all4.deb
-sleep 1
-apt-get -f install
-sleep 1
-apt update
-echo
-echo
-echo -e "\033[32m Das wars Openmediavult und Extras.org ist jetzt Installiert\033[0m"
-else echo -e "\033[31m Die Installation wurde abgebrochen\033[0m"
+  then 
+	sudo apt-get update &&
+	sleep 5
+	sudo apt-get upgrade -y &&
+	sleep 5
+	sudo apt-get mc install  python-requests python-setuptools python-dev build-essential libpcre3-dev libdvbcsa-dev git libcurl4-gnutls-dev debhelper cmake build-essential pkg-config libssl-dev gettext dvb-tools libdvbv5-0 liburiparser-dev liburiparser1 libavahi-client-dev zlib1g-dev libavcodec-dev libavutil-dev libavformat-dev libswscale-dev -y &&
+        sleep 5
+	cd /usr/src &&
+	sleep 5
+	sudo git clone -b release/4.2 https://github.com/tvheadend/tvheadend &&
+	sleep 5
+	cd tvheadend &&
+	sleep 5
+	./Autobuild.sh &&
+	sleep 5
+	
+	# install openmediavault
+	echo "deb http://packages.openmediavault.org/public arrakis main" > /etc/apt/sources.list.d/openmediavault.list &&
+	sleep 5
+	apt install --force-yes curl wget apt-transport-https dirmngr &&
+	sleep 5
+	gpg --recv-keys 7E7A6C592EF35D13 &&
+	sleep 5
+	gpg --export 7E7A6C592EF35D13 |apt-key add - &&
+	sleep 5
+	gpg --recv-keys 7E7A6C592EF35D13 &&
+	sleep 5
+	gpg --export 7E7A6C592EF35D13 |apt-key add - &&
+	sleep 5
+	gpg --recv-keys 24863F0C716B980B &&
+	sleep 5
+	gpg --export 24863F0C716B980B |apt-key add - &&
+	sleep 5
+	gpg --recv-keys 24863F0C716B980B &&
+	sleep 5
+	gpg --export 24863F0C716B980B |apt-key add - &&
+	sleep 1
+	apt update &&
+	sleep 1
+	apt-get install --force-yes openmediavault-keyring postfix ssl-cert &&
+	sleep 1
+	apt update &&
+	sleep 1
+	apt-get --yes --force-yes --allow-unauthenticated install openmediavault &&
+	sleep 1
+	wget http://omv-extras.org/openmediavault-omvextrasorg_latest_all4.deb
+	sleep 1
+	dpkg -i openmediavault-omvextrasorg_latest_all4.deb
+	sleep 1
+	apt-get -f install
+	sleep 1
+	apt update
+	echo
+	echo
+	echo -e "\033[32m Das wars Openmediavult und Extras.org ist jetzt Installiert\033[0m"
+else 
+	echo -e "\033[31m Die Installation wurde abgebrochen\033[0m"
 fi
-
-
 
